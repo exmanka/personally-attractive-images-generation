c6ce4e2b7b2dae54699047bcbbfffd7be6a50acc
Ким Михаил Алексеевич <kimma1@it.mos.ru>
2024-06-04 13:54:06 +0300

    ops(bot): update docker files

--- src/tgbot/database/build/init-database.sql
+++ src/tgbot/database/build/init-database.sql
@@ -12,3 +12,25 @@ CREATE TABLE users (
 
 CREATE INDEX users_idx
 ON users(telegram_id);
+
+
+CREATE TABLE models (
+	id SMALLSERIAL PRIMARY KEY,
+	name VARCHAR(32) UNIQUE NOT NULL
+);
+INSERT INTO models(name) VALUES('dcgan');
+INSERT INTO models(name) VALUES('stylegan3');
+
+
+CREATE TABLE generated_images (
+	id SERIAL PRIMARY KEY,
+	user_id INT UNIQUE NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+	model_id SMALLINT UNIQUE NOT NULL REFERENCES models(id) ON DELETE CASCADE,
+	rating SMALLINT NOT NULL,
+	image BYTEA NOT NULL,
+	seed BYTEA NOT NULL,
+	generation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+	constraint valid_rating
+		check (rating <= 10)
+);
